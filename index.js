44a94791b29d24ef1b7b038a4afb9ac4390ebb33
aaboothe <aaboothe73@gmail.com>
2021-11-22 18:08:44 -0800

    colony of cats

--- index.js
+++ index.js
@@ -1,14 +1,26 @@
 // Your Code Here
-window.prompt('Do you head left or right?')
+//let answer  = window.prompt('Do you head left or right?')
+//let firstAnswer  = window.prompt('Do you head left or right?')
+//leftconsole.log(firstAnswer)
+//let firstAnswer  = window.prompt('Do you head left or right?')
+//console.log(firstAnswer === 'left')
 let firstAnswer  = window.prompt('Do you head left or right?')
-if(firstAnswer === 'left'){
+if(firstAnswer === 'left') {
     let secondAnswer = window.prompt(`You come across a stray cat. 
 It scampers off down a small hole, just large enough for you to crawl through. 
 Do you follow it, or continue on your path?`)
-
-} else if(answer === 'right') {
+} 
+else if(firstanswer === 'right') {
     let secondAnswer = window.prompt(`You come across a snoring dragon. 
 On the other side of him, you see a shiny chest of treasure. Another path would 
 lead you away from the dragon altogether. Which path do you take?`)
-
 }
+let FirstAnswer  = window.prompt('Do you follow it, or continue on your path?')
+if(FirstAnswer === 'Follow') {
+    let secondAnswer = window.prompt(`You follow the cat to a colony of cats, 
+    nestled in a fort of warm blankets and subsisting off of inexplicably warm soup. 
+    They are content with you staying, but you wonder if you should alert the world to this magical safe haven.`)
+} 
+//else if(answer === 'Stay') {
+//    let secondAnswer = window.prompt(`You live happily amongst the cats for the rest of your days.`)
+//}
